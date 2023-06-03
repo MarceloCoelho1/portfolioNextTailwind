fd6a653f235983a255ebb1b17e28afd714fe6e72
Marcelo <mhbcoelho99@gmail.com>
2023-06-03 20:27:01 -0300

    portfolio page done

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,3 +1,10 @@
+import Image from 'next/image'
+import SocialPage from '../../assets/images/socialPage.png'
+import Bikecraft from '../../assets/images/bikecraft.png'
+import Animais from '../../assets/images/animais1.png'
+import NodeCrud from '../../assets/images/nodeCrud.png'
+import Algorithms from '../../assets/images/algorithms1.png'
+
 
 export default function portfolio() {
     return (
@@ -6,7 +13,9 @@ export default function portfolio() {
                 <h1 className="mb-5 text-xl font-medium">Front end</h1>
                 <div className="grid grid-cols-2 gap-8 justify-center align-top max-[450px]:grid-cols-1">
                     <div className=" flex flex-col items-center text-center">
-                        <div className="h-32 w-60 bg-white rounded-lg mb-1 max-sm:w-full max-[450px]:h-40"></div>
+                        <div className="h-32 w-60 rounded-lg mb-1 max-sm:w-full max-[450px]:h-48">
+                            <Image className='h-full w-full rounded-lg' src={SocialPage}/>
+                        </div>
                         <h1 className="mb-1 text-lg  font-normal">Dogs Social</h1>
                         <p className="text-sm">
                             social network specifically for dogs, similar to Instagram.
@@ -14,7 +23,9 @@ export default function portfolio() {
                     </div>
 
                     <div className=" flex flex-col items-center text-center">
-                        <div className="h-32 w-60 bg-white rounded-lg mb-1 max-sm:w-full max-[450px]:h-40"></div>
+                        <div className="h-32 w-60 rounded-lg mb-1 max-sm:w-full max-[450px]:h-48">
+                            <Image className='h-full w-full rounded-lg' src={Bikecraft}/>
+                        </div>
                         <h1 className="mb-1 text-lg font-normal">Bikecraft</h1>
                         <p className="text-sm">
                             e-commerce project built with HTML, CSS, and JavaScript
@@ -22,7 +33,9 @@ export default function portfolio() {
                     </div>
                    
                     <div className=" flex flex-col items-center text-center">
-                        <div className="h-32 w-60 bg-white rounded-lg mb-1 max-sm:w-full max-[450px]:h-40"></div>
+                        <div className="h-32 w-60 rounded-lg mb-1 max-sm:w-full max-[450px]:h-48">
+                            <Image className='h-full w-full rounded-lg' src={Animais}/>
+                        </div>
                         <h1 className="mb-1 text-lg font-normal">Animais api BTC</h1>
                         <p className="text-sm">
                             project developed using HTML, CSS, and JavaScript. By incorporating a Bitcoin API.
@@ -34,7 +47,9 @@ export default function portfolio() {
                 <h1 className="mb-5 text-xl font-medium">Back end</h1>
                 <div className="grid grid-cols-2 gap-8 justify-center align-top max-[450px]:grid-cols-1">
                     <div className=" flex flex-col items-center text-center">
-                        <div className="h-32 w-60 bg-white rounded-lg mb-1 max-sm:w-full max-[450px]:h-40"></div>
+                        <div className="h-32 w-60 rounded-lg mb-1 max-sm:w-full max-[450px]:h-48">
+                            <Image className='h-full w-full rounded-lg' src={NodeCrud}/>
+                        </div>
                         <h1 className="mb-1 text-lg font-normal">Crud with nodeJs and mySql</h1>
                         <p className="text-sm">
                             project developed using Node.js, MySQL, Express, Handlebars, and Bootstrap.
@@ -42,7 +57,7 @@ export default function portfolio() {
                     </div>
 
                     <div className=" flex flex-col items-center text-center">
-                        <div className="h-32 w-60 bg-zinc-800 rounded-lg mb-1 flex justify-center items-center max-sm:w-full max-[450px]:h-40">
+                        <div className="h-32 w-60 bg-zinc-800 rounded-lg mb-1 flex justify-center items-center max-sm:w-full max-[450px]:h-48">
                             <h1 className="text-xl font-semibold">coming soon</h1>
                         </div>
                         
@@ -54,7 +69,9 @@ export default function portfolio() {
                 <h1 className="mb-5 text-xl font-medium">Algorithms</h1>
                 <div className="grid grid-cols-2 gap-8 justify-center align-top max-[450px]:grid-cols-1">
                     <div className=" flex flex-col items-center text-center">
-                        <div className="h-32 w-60 bg-white rounded-lg mb-1 max-sm:w-full max-[450px]:h-40"></div>
+                        <div className="h-32 w-60 rounded-lg mb-1 max-sm:w-full max-[450px]:h-48">
+                            <Image className='h-full w-full rounded-lg' src={Algorithms}/>
+                        </div>
                         <h1 className="mb-1 text-lg font-normal">JavaAlgo</h1>
                         <p className="text-sm">
                             JavaAlgo is a repository created for me to store my Java-based algorithms and data structure codes
@@ -62,7 +79,7 @@ export default function portfolio() {
                     </div>
 
                     <div className=" flex flex-col items-center text-center">
-                        <div className="h-32 w-60 bg-zinc-800 rounded-lg mb-1 flex justify-center items-center max-sm:w-full max-[450px]:h-40">
+                        <div className="h-32 w-60 bg-zinc-800 rounded-lg mb-1 flex justify-center items-center max-sm:w-full max-[450px]:h-48">
                             <h1 className="text-xl font-semibold">coming soon</h1>
                         </div>
                     
