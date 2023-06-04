be562e8c72398375ec25e9a338b96895ed579cf3
Marcelo <mhbcoelho99@gmail.com>
2023-06-04 18:59:23 -0300

    production

--- app/portfolio/page.js
+++ app/portfolio/page.js
@@ -1,5 +1,5 @@
 import Header from '../../src/components/header/header'
-import CodingGif from '../../src/assets/gif/codingGif.gif'
+import CodingGif from '../../public/assets/gif/codingGif.gif'
 import Portfolio from '../../src/components/portfolio/portfolio'
 
 import Image from 'next/image'
